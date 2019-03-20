ec8bc6a43cd3420fd9d36f3de30d1047bb82334a
ibavli <ibrahim.bavli35@gmail.com>
2019-03-20 20:28:53 +0300

    Codility Time complexity ForgJmp

--- CodilityFrogJmp/Program.cs
+++ CodilityFrogJmp/Program.cs
@@ -10,6 +10,23 @@ namespace CodilityFrogJmp
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(solution(10, 85, 30).ToString());
+
+            Console.ReadLine();
+        }
+
+        public static int solution(int X, int Y, int D)
+        {
+            int minimalNumber = 0;
+
+            if (X < 1 || X > 1000000000 || Y < 1 || Y > 1000000000 || D < 1 || D > 1000000000 || X >= Y)
+                return minimalNumber;
+
+            minimalNumber = ((Y - X) / D);
+
+            if (((Y - X) % D) != 0) minimalNumber++;
+
+            return minimalNumber;
         }
     }
 }
